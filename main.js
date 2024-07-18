97a7560a8d1ac387e1ffb4df7a2064e82258a64f
e-grid.aika <aika.yuta@e-grid.co.jp>
2024-07-18 13:58:14 +0900

    [update]トリガー実行時間をスクリプトプロパティで設定するよう修正

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function main(){
-  //17時00分に関数cleaningDutyBotを実行するトリガーを作成
-  setTrigger('cleaningDutyBot',17,00);
+
+  //トリガーを実行する時間
+  const trigerHour = PropertiesService.getScriptProperties().getProperty("TrigerHour");
+  const trigerMinute = PropertiesService.getScriptProperties().getProperty("TrigerMinute");
+  
+  //trigerHour,trigerMinuteで指定した時間に関数cleaningDutyBotを実行するトリガーを作成
+  setTrigger('cleaningDutyBot',trigerHour,trigerMinute);
 }
