f5b164c4396066628123d22cfead7b293513a360
e-grid.aika <aika.yuta@e-grid.co.jp>
2024-08-13 14:40:29 +0900

    [update]TypeScript置き換えの為、設定を修正

--- main.js
+++
@@ -1,9 +0,0 @@
-function main(){
-
-  //トリガーを実行する時間
-  const trigerHour = PropertiesService.getScriptProperties().getProperty("TrigerHour");
-  const trigerMinute = PropertiesService.getScriptProperties().getProperty("TrigerMinute");
-  
-  //trigerHour,trigerMinuteで指定した時間に関数cleaningDutyBotを実行するトリガーを作成
-  setTrigger('cleaningDutyBot',trigerHour,trigerMinute);
-}
